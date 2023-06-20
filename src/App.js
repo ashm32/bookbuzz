f130b19676db07020d0432a9e2fc2bdd168ff9a8
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-20 21:45:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import { checkAuthStatus } from './actions/authActions';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
-
-const App = () => {
-  useEffect(() => {
-    store.dispatch(checkAuthStatus());
-  }, []);
-
-  return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route exact path="/login" component={LoginForm} />
-          <Route exact path="/register" component={RegisterForm} />
-        </Switch>
-      </Router>
-    </Provider>
-  );
-};
-
-export default App;
+const initialState = {
+    user: null,
+    error: null,
+  };
+  
+  const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'LOGIN_SUCCESS':
+        return {
+          ...state,
+          user: action.payload,
+          error: null,
+        };
+      case 'LOGIN_ERROR':
+        return {
+          ...state,
+          user: null,
+          error: action.payload,
+        };
+      // Implement other cases: REGISTER_SUCCESS, REGISTER_ERROR, LOGOUT_SUCCESS, LOGOUT_ERROR, etc.
+      default:
+        return state;
+    }
+  };
+  
+  export default authReducer;
+  
