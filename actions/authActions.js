0cc840d808d8072c5b51e210b4f1a98e1f8a5e04
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-20 21:26:19 +0100

    Update authActions.js
     Implemented other actions: register, logout, and checkAuthStatus

--- actions/authActions.js
+++ actions/authActions.js
@@ -1,21 +1,63 @@
 import firebase from '../firebase';
 
+// Action to handle user login
 export const login = (email, password) => {
   return (dispatch) => {
-    // Call the Firebase login method and handle the authentication process
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
-        // User login successful
         const user = userCredential.user;
         dispatch({ type: 'LOGIN_SUCCESS', payload: user });
       })
       .catch((error) => {
-        // Handle login error
         dispatch({ type: 'LOGIN_ERROR', payload: error.message });
       });
   };
 };
 
-// TODO: Implement other actions: register, logout, and checkAuthStatus
+// Action to handle user registration
+export const register = (email, password) => {
+  return (dispatch) => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        dispatch({ type: 'REGISTER_SUCCESS', payload: user });
+      })
+      .catch((error) => {
+        dispatch({ type: 'REGISTER_ERROR', payload: error.message });
+      });
+  };
+};
+
+// Action to handle user logout
+export const logout = () => {
+  return (dispatch) => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        dispatch({ type: 'LOGOUT_SUCCESS' });
+      })
+      .catch((error) => {
+        dispatch({ type: 'LOGOUT_ERROR', payload: error.message });
+      });
+  };
+};
+
+// Action to check the authentication status
+export const checkAuthStatus = () => {
+  return (dispatch) => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is authenticated
+        dispatch({ type: 'LOGIN_SUCCESS', payload: user });
+      } else {
+        // User is not authenticated
+        dispatch({ type: 'LOGOUT_SUCCESS' });
+      }
+    });
+  };
+};
