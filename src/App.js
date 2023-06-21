d571ab8330ddaeeaa8b69069a99f5c12b78f61b3
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-21 22:38:36 +0100

    Update App.js
    updated to include nav bar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
+import BookDetails from './components/BookDetails';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
-import BookDetails from './components/BookDetails';
+import './App.css'; // Import the CSS file for styling
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/book/:id" component={BookDetails} />
-        </Switch>
+        <div>
+          <Navbar /> {/* Include the Navbar component */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/book/:id" component={BookDetails} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+          </Switch>
+        </div>
       </Router>
     </Provider>
   );
