64b76dc2217aa379044428b9d9f30fd96b0b95ce
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-21 18:15:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import { checkAuthStatus } from './actions/authActions';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
+import { checkAuthStatus } from '/Users/ayshahmalik/Downloads/bookbuzz/actions/authActions.js';
+import LoginForm from '/Users/ayshahmalik/Downloads/bookbuzz/components/LoginForm';
+import RegisterForm from '/Users/ayshahmalik/Downloads/bookbuzz/components/RegisterForm';
+import Books from '/Users/ayshahmalik/Downloads/bookbuzz/components/Books.js'; // Import the new component
 
 const App = () => {
-  useEffect(() => {
-    store.dispatch(checkAuthStatus());
-  }, []);
+  // Dispatch checkAuthStatus action as needed
 
   return (
     <Provider store={store}>
@@ -17,6 +16,8 @@ const App = () => {
         <Switch>
           <Route exact path="/login" component={LoginForm} />
           <Route exact path="/register" component={RegisterForm} />
+          <Route exact path="/books" component={Books} /> // Add the new route
+          {/* Add other routes for your application */}
         </Switch>
       </Router>
     </Provider>
