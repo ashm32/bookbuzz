9cb3bd5a4268d87832207832260b26c71955cf90
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-21 22:21:19 +0100

    Create types.js
    defined constants for different action types

---
+++ actions/types.js
@@ -0,0 +1,8 @@
+export const ADD_REVIEW = 'ADD_REVIEW';
+export const EDIT_REVIEW = 'EDIT_REVIEW';
+export const DELETE_REVIEW = 'DELETE_REVIEW';
+
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGOUT = 'LOGOUT';
+export const CHECK_AUTH_STATUS = 'CHECK_AUTH_STATUS';
