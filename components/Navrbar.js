c85c15df63fdf33f1569cb99a677c3195172c01f
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-21 22:40:29 +0100

    Create Navrbar.js

---
+++ components/Navrbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Import the CSS file for styling
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">My Book Catalog</Link> {/* Update the link destination as needed */}
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
