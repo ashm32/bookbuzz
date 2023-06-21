1805f2d092bda4697fff6a120781a2dfc3fa8e08
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-21 18:18:15 +0100

    Update index.js
    imported redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { combineReducers } from 'redux';
+import authReducer from '/actions/authActions.js';
+import booksReducer from '/actions/booksReducer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootReducer = combineReducers({
+  auth: authReducer,
+  books: booksReducer, // Add the new reducer
+  // Add other reducers if needed
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default rootReducer;
