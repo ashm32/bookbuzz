1c4600ea03502e719ee497c00f76c7c87b22947c
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-21 19:21:26 +0100

    Update booksReducer.js
    updated to handle the new action for fetching book details

--- reducers/booksReducer.js
+++ reducers/booksReducer.js
@@ -2,12 +2,16 @@ import {
   FETCH_BOOKS_REQUEST,
   FETCH_BOOKS_SUCCESS,
   FETCH_BOOKS_FAILURE,
-} from '/actions/authActions.js';
+  FETCH_BOOK_DETAILS_REQUEST,
+  FETCH_BOOK_DETAILS_SUCCESS,
+  FETCH_BOOK_DETAILS_FAILURE,
+} from '../actions/booksActions';
 
 const initialState = {
   books: [],
   loading: false,
   error: null,
+  bookDetails: null,
 };
 
 const booksReducer = (state = initialState, action) => {
@@ -25,6 +29,25 @@ const booksReducer = (state = initialState, action) => {
         books: action.payload,
       };
     case FETCH_BOOKS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case FETCH_BOOK_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+        bookDetails: null,
+      };
+    case FETCH_BOOK_DETAILS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        bookDetails: action.payload,
+      };
+    case FETCH_BOOK_DETAILS_FAILURE:
       return {
         ...state,
         loading: false,
