60cd5eef073b8d7093cd2ca9e1678ffd832b7b04
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-22 20:21:15 +0100

    Update index.js
    added to to handle the error state

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
+import errorReducer from './errorReducer';
 import authReducer from './authReducer';
 import reviewReducer from './reviewReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   reviews: reviewReducer,
+    error: errorReducer,
 });
 
 export default rootReducer;
+
+
